fe981802461dbea04e97231b25c1187bd06c66e3
cameronotoole44 <cameronotoole404@gmail.com>
2024-06-08 00:03:38 -0400

    cleanup config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
-
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      fontFamily: {
+        'press-start': ['"Press Start 2P"', 'cursive']
+      },
+      colors: {
+        'violet-800': '#5A4E7C',
+        'violet-700': '#6B5D92',
+        'blue-500': '#4A90E2',
+        'teal-400': '#4FD1C5',
+        'gray-800': '#2D3748',
+        'gray-700': '#4A5568',
+        'gray-600': '#718096',
+        'gray-200': '#EDF2F7',
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
