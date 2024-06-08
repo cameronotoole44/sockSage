1df347bb41c4724e0e3112c8bce4bc77adfe0432
cameronotoole44 <cameronotoole404@gmail.com>
2024-06-07 20:12:19 -0400

    animation added to img

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import RandomQuestion from './components/RandomQuestion';
 function App() {
   const [sockColors, setSockColors] = useState(['Black', 'White', 'Multi-color/Pattern']);
   const [selectedColor, setSelectedColor] = useState('');
+  const [animate, setAnimate] = useState(false);
 
   const pickSockColor = () => {
-    const randomIndex = Math.floor(Math.random() * sockColors.length);
-    setSelectedColor(sockColors[randomIndex]);
+    const randomIndex = Math.floor(Math.random() * sockColors.length)
+    setSelectedColor(sockColors[randomIndex])
+    setAnimate(true)
+    setTimeout(() => {
+      setAnimate(false)
+    }, 20000)
   };
 
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-b from-blue-800 via-blue-700 to-blue-500 font-montserrat">
       <div className="text-center bg-gradient-to-b from-white to-gray-200 p-5 border-2 border-gray-600 rounded-lg shadow-md max-w-lg w-full">
         <h1 className="font-press-start text-6xl font-normal mb-5 text-gray-800 shadow-lg">
-          SOCK SAGE <img src={`${process.env.PUBLIC_URL}/sage.png`} alt="Sage" className="inline-block w-16 h-16 ml-3" />
+          SOCK SAGE <img src={`${process.env.PUBLIC_URL}/sage.png`} alt="Sage" className={`inline-block w-16 h-16 ml-3 ${animate ? 'animate-bounce' : ''}`} />
         </h1>
         <RandomQuestion />
         <button
